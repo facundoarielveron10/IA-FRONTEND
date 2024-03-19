a2e38481935ebb5ca05d6aed47b4aa1d01c3a88c
Facundo Veron <facu_ve_2016@hotmail.com>
2024-03-19 16:20:28 -0300

    Creando el Frontend y Conectando con la API de OpenAI API

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import { useState } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [prompt, setPrompt] = useState('');
+    const [response, setResponse] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        axios
+            .post('http://localhost:8080/chat', { prompt })
+            .then((res) => {
+                setResponse(res.data);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    };
+
+    return (
+        <>
+            {/* CONTENIDO */}
+            <div className="content">
+                {/* CONTENEDOR */}
+                <div className="container">
+                    {/* FORMULARIO */}
+                    <form className="form" onSubmit={handleSubmit}>
+                        {/* INFORMACION */}
+                        <div className="info">
+                            <label className="label" htmlFor="info">
+                                Pregunta o di algo...
+                            </label>
+                        </div>
+                        {/* ENTRADA DE TEXTO */}
+                        <div className="field">
+                            <input
+                                className="input"
+                                type="text"
+                                value={prompt}
+                                onChange={(e) => setPrompt(e.target.value)}
+                            />
+                        </div>
+                        {/* BOTON DE ENVIAR */}
+                        <div>
+                            <button className="submit" type="submit">
+                                Enviar
+                            </button>
+                        </div>
+                    </form>
+                    {/* RESPUESTA DE LA API */}
+                    <div className="response">
+                        <p>{response}</p>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
 }
 
 export default App;
